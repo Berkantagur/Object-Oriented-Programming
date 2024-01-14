0cd59cef54048fa070b7767e64c794773e82fae7
Berkantagur <berkantagur6@gmail.com>
2024-01-14 20:29:48 +0300

    .cpp

--- OOP Final/2022/Q4.cpp
+++ OOP Final/2022/Q4.cpp
@@ -1,7 +1,7 @@
 // Write the program code for overloading the operator " = " (assignment operator) for class class1 which is declared below.
 // dim_a is size the double type vector that the pointer " *A " points. You must take into account whether sizes of the vectors
 // of the objects are same or not while overloading the operator " = "
-class class1
+/*class class1
 {
 public:
     int dim_a;
@@ -16,3 +16,25 @@ public:
         }
         class1 &operator=(class1 &object_1); // " = " declaration of the operator " = "
     }
+*/
+/*
+class class1 &operator=(class1 &object_1){
+    if (dim_a == object_1.dim_a)
+    {
+        for (int i = 0; i < dim_a; i++)
+        {
+            A[i] = object_1.A[i];
+        }
+    }
+    else
+    {
+        delete[] A;
+        dim_a = object_1.dim_a;
+        A = new double[dim_a];
+        for (int i = 0; i < dim_a; i++)
+        {
+            A[i] = object_1.A[i];
+        }
+    }
+    return *this;
+}*/
